7a3f16a199d64dbb4dd8397b6c12ce2908fbad4a
Anagha Suresh <anaghas2001@gmail.com>
2025-11-08 18:52:36 -0500

    createdb statement

---
+++ database/create_table.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS movemind_db;
+
+USE movemind_db;
+
+CREATE TABLE User(
+id INT AUTO_INCREMENT PRIMARY KEY,
+firstname  VARCHAR(50) NOT NULL,
+lastname   VARCHAR(50) NOT NULL,
+email VARCHAR(100) NOT NULL UNIQUE ,
+password_hash VARCHAR(255) NOT NULL,
+creation_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+INDEX idx_email(email));
